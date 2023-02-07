24aae280dc32e2a31c30a904a39f8070549611a2
Thiago Rodrigues <thiago.alvimrodrigues@gmail.com>
2023-02-07 13:52:07 -0300

    página Login completa

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,15 @@
+import LoginPageApp from "../components/LoginApp/Index";
+
+
+function AppLogin() {
+  return (
+    <>
+    <div style={{minHeight: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", background: "rgba(255, 255, 200)"}}>
+        <LoginPageApp/>
+    </div>
+    </>
+        
+  )
+}
+
+export default AppLogin
