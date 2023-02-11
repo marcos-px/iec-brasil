d1efa5cf1a146b2df7a3c1af022931af3e581ee6
Tarik Raposo Parreira <tarikraposo@gmail.com>
2023-02-10 21:18:03 -0300

    editing-contact
    Continuando a estilização da página de contato.

--- src/components/Contact/Contact.ts
+++ src/components/Contact/Contact.ts
@@ -7,6 +7,7 @@ export const ContactForm = styled.form`
   align-items: center;
   flex-direction: column;
   height: 100vh;
+  width: 60vh;
   color: #fff;
   padding: 14px;
   color: #000;
@@ -34,26 +35,127 @@ export const ContactForm = styled.form`
   }
   .textarea {
     margin-bottom: 14px;
-    border-radius: 4px;
     border: 0;
+    border-bottom: 1px solid #8f8f8f;
     padding: 8px;
-    height: 94px;
+    height: 38px;
     resize: none;
+    margin-top: 20px;
+    background-color: transparent;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 150%;
+    overflow: hidden;
+  }
+  .textarea::placeholder {
+    font-family: "Archivo";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+  }
+  .textarea:active {
+    background-color: transparent;
   }
 
   .button {
-    height: 34px;
+    left: 80%;
+    top: 5%;
+    position: relative;
+    width: 94px;
+    height: 48px;
     border-radius: 4px;
     border: 0;
+    color: #fff;
     cursor: pointer;
-    background-color: #0fdbff;
-    font-size: 18px;
+    background-color: #1ca3dd;
+    font-size: 16px;
     transition: background-color, transform 0.8s;
   }
 
   .button:hover {
     background-color: #0fe3ff;
     transform: scale(1.01);
+  }
+  .checkbox-control {
+    display: flex;
+    justify-content: left;
+    gap: 30px;
+  }
+
+  // INPUT CHECKBOX//
+
+  [type="checkbox"]:not(:checked),
+  [type="checkbox"]:checked {
+    position: absolute;
+    left: -9999px;
+  }
+  [type="checkbox"]:not(:checked) + label,
+  [type="checkbox"]:checked + label {
+    position: relative;
+    padding-left: 30px;
+    cursor: pointer;
+    margin-bottom: 24px;
+    margin-top: 24px;
+    display: inline-block;
+    font-size: 14px;
+  }
+  /* checkbox aspect */
+  [type="checkbox"]:not(:checked) + label:before,
+  [type="checkbox"]:checked + label:before {
+    content: "";
+    position: absolute;
+    left: 0px;
+    top: -3px;
+    width: 22px;
+    height: 22px;
+    border: 1px solid #000000;
+    background: #fff8f0;
+    border-radius: 4px;
+    box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.1);
+  }
+  /* checked mark aspect */
+  [type="checkbox"]:not(:checked) + label:after,
+  [type="checkbox"]:checked + label:after {
+    content: "✔";
+    position: absolute;
+    top: 2px;
+    left: 5px;
+    font-size: 20px;
+    line-height: 1.2;
+    color: #0000ff;
+    transition: all 0.2s;
+  }
+  /* checked mark aspect changes */
+  [type="checkbox"]:not(:checked) + label:after {
+    opacity: 0;
+    transform: scale(0);
+  }
+  [type="checkbox"]:checked + label:after {
+    opacity: 1;
+    transform: scale(1);
+  }
+  /* disabled checkbox */
+  [type="checkbox"]:disabled:not(:checked) + label:before,
+  [type="checkbox"]:disabled:checked + label:before {
+    box-shadow: none;
+    border-color: #999999;
+    background-color: #dddddd;
+  }
+  [type="checkbox"]:disabled:checked + label:after {
+    color: #999999;
+  }
+  [type="checkbox"]:disabled + label {
+    color: #aaaaaa;
+  }
+  /* accessibility */
+  [type="checkbox"]:checked:focus + label:before,
+  [type="checkbox"]:not(:checked):focus + label:before {
+    border: 2px dotted #0000ff;
+  }
+  /* hover style just for information */
+  label:hover:before {
+    border: 2px solid #4778d9 !important;
+    background: #fff8f0;
   }
 `;
 
@@ -64,6 +166,7 @@ export const ContactInfo = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
   color: white;
+  width: 40vh;
   max-height: 100vh;
   height: 680px;
   width: 470px;
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,109 @@
+import { Link } from "react-router-dom";
+import Logo from "../../../public/iec.png";
+import { BiEnvelope } from "react-icons/bi";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import {
+  AiOutlineArrowRight,
+  AiOutlineInstagram,
+  AiOutlineTwitter,
+  AiFillYoutube,
+} from "react-icons/ai";
+import { FooterStyle, InfoFooter } from "./Footer.style";
+
+function Footer() {
+  return (
+    <FooterStyle>
+      <InfoFooter>
+        <div className="ContainerNavecacaoFooter">
+          <div className="ContainterLogo">
+            <Link to={""}>
+              <img src={Logo} alt="" />
+            </Link>
+          </div>
+          <div className="NavecacaoFooter">
+            <h3>Navegação</h3>
+            <ul>
+              <li>
+                <Link to={""}>Home</Link>
+              </li>
+              <li>
+                <Link to={""}>Proeduc</Link>
+              </li>
+              <li>
+                <Link to={""}>Projetos</Link>
+              </li>
+              <li>
+                <Link to={""}>Blog</Link>
+              </li>
+              <li>
+                <Link to={""}>Contato</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="NavecacaoFooter">
+            <h3>Termos e Políticas</h3>
+            <ul>
+              <li>
+                <Link to={""}>Termos de uso</Link>
+              </li>
+              <li>
+                <Link to={""}>Política de privacidade</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="ContainerContatoFooter">
+          <label>
+            Newslatter
+            <div>
+              <input type="text" placeholder="Email" />
+              <i>
+                <BiEnvelope />
+              </i>
+              <button>
+                <BiEnvelope />
+                <AiOutlineArrowRight />
+              </button>
+            </div>
+          </label>
+          <div className="NavegacaoRedesSociais">
+            <ul>
+              <li>
+                <Link to={""}>
+                  <AiOutlineInstagram />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiOutlineTwitter />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaFacebookF />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <FaLinkedinIn />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <AiFillYoutube />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </InfoFooter>
+      <div className="RodapeFooter">
+        <p>
+          2023 Instituto de Educação e Cidadania.Todos os direitos reservados
+        </p>
+      </div>
+    </FooterStyle>
+  );
+}
+
+export default Footer;
